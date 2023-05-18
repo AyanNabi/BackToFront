d28f6bcc5b6c3c3f812122e0a8524f08cd1a24db
AyanNabi <ayanmna@code.edu.az>
2023-05-18 23:28:31 +0400

    feat:First part is added

--- FrontToBack/FrontToBack/Controllers/HomeController.cs
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace FrontToBack.Controllers
             HomeVM homevm = new HomeVM();
             homevm.SliderImages = _appDbContext.SliderImages.ToList();
             homevm.SliderContent = _appDbContext.SliderContent.FirstOrDefault();
-            homevm.Products = _appDbContext.Products.ToList();
+            homevm.Products = _appDbContext.Products.Include(p=>p.ProductImages).ToList();
             homevm.Categories = _appDbContext.Categories.ToList();
             return View(homevm);
         }
