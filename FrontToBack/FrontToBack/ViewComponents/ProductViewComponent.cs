b48cf1fb0d25578bf37446f81fafbb4aeb9cf37e
AyanNabi <ayanmna@code.edu.az>
2023-05-23 23:31:10 +0400

    feat:View Components are added

---
+++ FrontToBack/FrontToBack/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using FrontToBack.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewCompanents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        readonly AppDbContext _appDbContext;
+        public ProductViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = _appDbContext.Products
+                .Include(p=>p.ProductImages)
+                .Include(p=>p.category)
+                .Take(4)
+                .ToList();
+            return View(await Task.FromResult(products));  
+
+        }
+    }
+}
+
